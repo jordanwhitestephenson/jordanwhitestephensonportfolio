9df668de2aa52b7f4fa4d9b5f30d7296641fe300
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-03-12 12:29:53 -0600

    portfolio

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8000
+const bodyParser = require('body-parser')
+
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static('public'));
+
+app.listen(PORT, ()=> {
+  console.log(`Listening on port ${PORT}`)
+})
+
+module.exports = app
