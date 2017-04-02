c924a1df70bcd48d00209b922cfff3182d025561
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-04-02 16:46:35 -0600

    figuring out folder placement

--- SASS Practive/app.js
+++ SASS Practive/app.js
@@ -3,14 +3,14 @@ const app = express()
 const PORT = process.env.PORT || 8000
 const bodyParser = require('body-parser')
 const sass = require('sass')
-const bourbon = require('bourbon')
+
 
 
 
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.uses(bourbon)
+
 
 app.use(express.static('public'));
 
@@ -18,4 +18,4 @@ app.listen(PORT, ()=> {
   console.log(`Listening on port YA! ${PORT}`)
 })
 
-module.exports = app, bourbon
+module.exports = app
